3afd2075632159d43525cac3a8cded1781dc28f1
Rene Mbanguka <rmbanguka@gmail.com>
2023-11-15 02:08:37 +0100

    Initial Commit

---
+++ Uppgift2Appen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Uppgift2Appen
+//
+//  Created by Rene Mbanguka on 2023-11-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Uppgift2Appen/Uppgift2AppenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Uppgift2AppenApp.swift
+//  Uppgift2Appen
+//
+//  Created by Rene Mbanguka on 2023-11-15.
+//
+
+import SwiftUI
+
+@main
+struct Uppgift2AppenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
