c6a5e13ac86fcd4c5a5f17d3f933bf74b7100740
Rene Mbanguka <rmbanguka@gmail.com>
2023-11-15 04:07:20 +0100

    Uppgift2 - complete

--- Uppgift2Appen/ContentView.swift
+++ Uppgift2Appen/ContentView.swift
@@ -4,19 +4,63 @@
 //
 //  Created by Rene Mbanguka on 2023-11-15.
 //
-
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var addText = ""
+    
+    @State var textTable = [String]()
+    
     var body: some View {
+        
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            HStack{
+                
+                TextField("Striv text", text: $addText)
+                    .font(/*@START_MENU_TOKEN@*/.title2/*@END_MENU_TOKEN@*/)
+                
+                Button(action:{
+                    if(addText != "") {
+                        textTable.append(addText)
+                        addText = ""
+                    }
+                    
+                }) {
+                    Text("Lägg till")
+                        .font(.title2)
+                }
+                
+            }
+            
+            List (textTable, id: \.self) { rowItem in
+                Text(rowItem)
+                    .font(.body)
+                
+            }
+            
+            Button(action:{
+                
+                textTable = []
+                
+            }) {
+                Text("Nollställ")
+                    .font(.title2)
+                    .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.orange/*@END_MENU_TOKEN@*/)
+                
+            }
+            
+            .disabled(textTable.isEmpty)
+            
         }
+        
         .padding()
+        .navigationTitle("Välkommen")
+        
     }
+    
 }
 
 #Preview {
